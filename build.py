0672c489724291020ca9fb745b816f2de1d2fe26
SevenDreamer <seven@dreamer.dev>
2026-04-27 14:36:23 +0800

    fix: add custom_target_triple for new flutter engine

--- build.py
+++ build.py
@@ -162,6 +162,7 @@ class Build:
             '--gn-args', 'is_termux=true',
             '--gn-args', f'is_termux_host={utils.__TERMUX__}',
             '--gn-args', f'termux_api_level={api}',
+            '--gn-args', 'custom_target_triple="aarch64-linux-android"',
         ]
         subprocess.run(cmd, cwd=root, check=True, stdout=True, stderr=True)
 
