f75780e147333a38b96eb6da25a980548b4275a6
qcloud <ubuntu@localhost.localdomain>
2026-05-01 13:39:27 +0800

    fix: add glibconfig.h and atk header include paths for GTK build

--- build.py
+++ build.py
@@ -243,6 +243,7 @@ config("sdk") {
     cflags += [ "-I$custom_sysroot/usr/include/glib-2.0" ]
     cflags += [ "-I$custom_sysroot/usr/lib/glib-2.0/include" ]
     cflags += [ "-I$custom_sysroot/usr/include/gtk-3.0" ]
+    cflags += [ "-I$custom_sysroot/usr/include/atk-1.0" ]
     cflags += [ "-I$custom_sysroot/usr/include/pango-1.0" ]
     cflags += [ "-I$custom_sysroot/usr/include/cairo" ]
     cflags += [ "-I$custom_sysroot/usr/include/gdk-pixbuf-2.0" ]
